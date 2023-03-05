0c8f2835a7a98e7c0b0cd10f2b64e817bffeff17
Mikhail Sergeev <sergeevm100@gmail.com>
2023-03-05 14:14:13 +0300

    added styles for pagination

--- src/entities/table/tableEntity.vue
+++ src/entities/table/tableEntity.vue
@@ -28,27 +28,75 @@ const setPage = (page: number) => store.commit('table/settings/setPage', page);
 const page = computed(() => store.getters['table/settings/paginationPage']);
 </script>
 <template>
-  <div class="table-wrapper">
-    <table-navigation :activeTab="activeTab" @changeTab="changeTab" />
-    <component :is="component.name" />
-    <div class="pagination">
-      <button :disabled="page === 0" @click="setPage(page - 1)">Назад</button>
-      <button :disabled="page === 3" @click="setPage(page + 1)">Вперед</button>
+  <div class="table-page">
+    <div class="table-wrapper">
+      <table-navigation :activeTab="activeTab" @changeTab="changeTab" />
+      <component :is="component.name" />
+    </div>
+    <div class="pagination" v-if="activeTab === 'find'">
+      <button class="back-btn" :disabled="page === 0" @click="setPage(page - 1)">
+        <i class="fa-solid fa-chevron-left"></i>
+      </button>
+      <button class="page" v-if="page !== 0" @click="setPage(page - 1)">
+        {{ page }}
+      </button>
+      <button class="page page-current">{{ page + 1 }}</button>
+      <button class="page" v-if="page !== 3" @click="setPage(page + 1)">
+        {{ page + 2 }}
+      </button>
+      <button class="next-btn" :disabled="page === 3" @click="setPage(page + 1)">
+        <i class="fa-solid fa-chevron-right"></i>
+      </button>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.table-page {
+  width: 100%;
+  display: grid;
+  margin: 10px 10px 0;
+}
 .table-wrapper {
   height: fit-content;
-  margin: 10px 10px 0;
   border: 1px solid #ebebeb;
   width: 100%;
   position: relative;
 }
 .pagination {
-  margin-top: 10px;
+  margin: 10px 0;
   display: flex;
+  justify-content: end;
   gap: 10px;
+}
+.back-btn,
+.next-btn {
+  width: 30px;
+  height: 30px;
+  border: 0;
+  border-radius: 100%;
+  background: none;
+  cursor: pointer;
+  transition: 0.3s;
+  &:hover {
+    background-color: #ebebeb;
+  }
+}
+.next-btn {
+}
+.page {
+  line-height: 30px;
+  width: 30px;
+  height: 30px;
+  border: 0;
+  border-radius: 100%;
+  background: none;
+  cursor: pointer;
+  &-current {
+    background: #f6f6f6;
+  }
+  &:hover {
+    background-color: #ebebeb;
+  }
 }
 </style>
